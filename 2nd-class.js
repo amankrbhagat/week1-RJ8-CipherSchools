020c3b445a7a888fbf30b46100a0417d10292d00
amankrbhagat <115861752+amankrbhagat@users.noreply.github.com>
2023-01-23 01:33:51 +0530

    Add files via upload

--- 2nd-class.js
+++ 2nd-class.js
@@ -135,25 +135,25 @@
 
 // Class
 
-class Animal {
-    noOfLegs;
-    color;
-    family;
-    sound;
+// class Animal {
+//     noOfLegs;
+//     color;
+//     family;
+//     sound;
 
-    constructor(noOfLegs, color, family, sound) {
-        this.noOfLegs = noOfLegs;
-        this.color = color;
-        this.family = family;
-        this.sound = sound;
-    }
-    showAnimal() {
-        console.log(`The animal belongs to family ${this.family}`);
-    }
-}
+//     constructor(noOfLegs, color, family, sound) {
+//         this.noOfLegs = noOfLegs;
+//         this.color = color;
+//         this.family = family;
+//         this.sound = sound;
+//     }
+//     showAnimal() {
+//         console.log(`The animal belongs to family ${this.family}`);
+//     }
+// }
 
-let animal = new Animal(4, "brown", "rodent", "Phew Phew");
-//console.log(animal);
-animal.showAnimal();
+// let animal = new Animal(4, "brown", "rodent", "Phew Phew");
+// //console.log(animal);
+// animal.showAnimal();
 
 
