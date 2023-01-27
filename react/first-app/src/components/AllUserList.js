5b1318f145ecda97465211238eea0a4fe40ddc4a
amankrbhagat <115861752+amankrbhagat@users.noreply.github.com>
2023-01-27 22:26:02 +0530

    Add files via upload

--- react/first-app/src/components/AllUserList.js
+++ react/first-app/src/components/AllUserList.js
@@ -1,21 +1,69 @@
-import React from "react";
+import React,{ useState } from "react";
+import Test from  "./Test";
+import UserDetails from "./UserDetails";
+
 
 const AllUserList = () => {
-    let allUsers = [
+    const [state, setState] = useState({
+        name: "Akb",
+    });
+    // state = {
+    //     name: "Priyanshu Saxena",
+    // };
+    //allUsers: [
+        let allUsers =[
         {name: "Aman", email: "aman@email.com", phoneNumber: "1223445323"},
+        
         {name: "Ankit", email: "ankit@email.com", phoneNumber: "1223565323"},
         {name: "Ajay", email: "ajay@email.com", phoneNumber: "1583445323"},
-
- 
-
     ];
+
+
+    // let array = [
+    //     <UserDetails key={0} user={allUsers[0]} />,
+    //     <UserDetails key={1} user={allUsers[1]} />,
+    //     <UserDetails key={2} user={allUsers[2]} />,
+
+    // ];
+    // return (
+    //     <div>
+    //     <userDetails user={allUsers[0]} />
+    //     <userDetails user={allUsers[1]} />
+    //     <userDetails user={allUsers[2]} />
+    //     </div>
+    // )
+
+    // render() {
+    //     setTimeout(() => {
+            
+    //      (() => {
+    //         this.setState({name: "Abhay Raj Gupta" });
+    //     }, 15000);
+
     return (
         <div>
-        <userDetails user={allUsers[0]} />
-        <userDetails user={allUsers[1]} />
-        <userDetails user={allUsers[2]} />
+            <Test name = {state.name} />
+            {allUsers.map((user, index) => (
+                {/* {this.allusers.map((user, index) => ( */}
+                <UserDetails key={index} user={user} />
+            ))}
         </div>
-    )
-};
+    );
+ };
+       
+//         return (
+//             <div>
+//                 <h1>The name is {this.state.name}</h1>
+//                 {this.allUsers.map((user, index) => (
+//                     <UserDetails key={index} user={user} />
+//                 ))}
+//             </div>
+//         );
+//     }
+// }
+
+// const AllUserList = () => {
+//     let [state, setState] = useState( {name: "Priyanshu Saxena"});
+// };
 
 export default AllUserList;
